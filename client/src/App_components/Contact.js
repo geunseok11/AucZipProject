c4f0355a08cfb6a0d2d5fd681a389c12ec579573
CreamPuffx2 <exploit017@gmail.com>
2020-07-01 16:28:59 +0900

    client/src/App_components MAKE App.js 컴포넌트 생성

---
+++ client/src/App_components/Contact.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class News extends Component {
+  render() {
+    <div>
+      <h2>Contact</h2>
+      <div>+82 0101234345</div>
+      <div>(주)우아한 아재들</div>
+      <img>mail : ggg@gmail.com</img>
+    </div>;
+  }
+}
