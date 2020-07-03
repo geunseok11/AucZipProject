21b4cb9b41220c31a5e80ab3f632aebba72faf97
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-07-03 17:19:06 +0900

    MyPage UPDATE 마이페이지 업데이트

--- client/src/pages/components/MyPage/MyInvestInfo.js
+++ client/src/pages/components/MyPage/MyInvestInfo.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import Deposit from './Deposit';
+import BuildingInfo from '../Index/BuildingProducts';
 ​
 export default class MyInvestInfo extends Component {
   redner() {
+    const { data } = this.props;
+
     return (
       <div>
-      
+
+        {data ? (
+          data.map((data_val, i) => <BuildingInfo key={i} {...data_val} />)
+        ) : (
+          <h1>투자내역이 없습니다</h1>
+        )}
       </div>
     ); 
   }
