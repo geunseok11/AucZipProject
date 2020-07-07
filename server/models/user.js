25c766a92e6466c089301e4046ba56cf26f2c8fd
조규창 <cho@jogyuchang-ui-MacBookPro.local>
2020-07-07 16:57:54 +0900

    temp fix

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('Users', {
+  const User = sequelize.define('User', {
     name: {
       type: DataTypes.STRING,
       allowNull: false
