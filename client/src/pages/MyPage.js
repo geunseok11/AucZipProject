0d16f9cf622b3e0025aad0d689049452a23e01c9
CreamPuffx2 <exploit017@gmail.com>
2020-07-08 16:17:38 +0900

    app.js에서 마이페이지 접속 불가 문제 수정

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,25 +1,24 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
 import MyInfo from "./components/MyPage/MyInfo";
 import MyInvestInfo from "./components/MyPage/MyInvestInfo";
 
-export default class MyPage extends Component {
-  render(props) {
-    if (this.props.isLogin) {
-      return (
-        <div>
-          <h1>Mypage</h1>
-          <MyInfo /><br/>
-        <MyInvestInfo /> <br/>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1>NOT FOUND</h1>
-        </div>
-      );
-    }
+const MyPage = ({ isLogin }) => {
+  if (isLogin) {
+    return (
+      <div>
+        <h1>Mypage</h1>
+        <MyInfo />
+        <br />
+        <MyInvestInfo /> <br />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>NOT FOUND</h1>
+      </div>
+    );
   }
-}
+};
+
+export default MyPage;
