e52c9d1aff9147a9db507e4510cdde89c0c534a6
ChavezEL <hjleesure@gmail.com>
2020-07-09 11:11:08 +0900

    server UPDATE building info user info 데이터 수정

--- server/controller/building/info.js
+++ server/controller/building/info.js
@@ -3,7 +3,7 @@ const { Buildings } = require('../../models');
 module.exports = {
     get: (req, res) => {
         Buildings.findAll({
-            attributes: ['b_name', 'image', 'b_evaluation', 'b_invest', 'b_invest_goal', 'b_invest_user_num',
+            attributes: ['id', 'b_name', 'image', 'b_evaluation', 'b_invest', 'b_invest_goal', 'b_invest_user_num',
         'b_info', 'b_location', 'b_use', 'b_size', 'b_due', 'b_views', 'b_invest_start_date',
         'b_invest_end_date']
           })
