57cbd300c8a735636d8955cbc6465b1b2bb82ccb
조규창 <cho@jogyuchang-ui-MacBookPro.local>
2020-07-09 13:22:50 +0900

    temp

--- client/src/pages/components/Buliding/buildingList.js
+++ client/src/pages/components/Buliding/buildingList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, Route, withRouter } from "react-router-dom";
 import BuildingItem from "./buildingItem";
-
+import Invest from './invest'
 const BuildingList = (props) => (
   <div>
     {props.buildings.map((data) => {
